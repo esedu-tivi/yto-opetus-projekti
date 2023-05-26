139e63a134eaffda15b62e7ad8697e78d8f70ad8
JutilaO <46874717+JutilaO@users.noreply.github.com>
2023-05-26 08:53:23 +0300

    parametrit lopuille tehtäville

--- scripts/physics.js
+++ scripts/physics.js
@@ -1,16 +1,27 @@
-document.getElementById("task-dropdown").addEventListener("click", event => taskSelector(event))
+document.getElementById('task-dropdown').addEventListener('click', event => taskSelector(event))
 
 const canvas_div = document.getElementById('canvas_div')
-canvas_div.style.backgroundColor = "#A800D9"
+canvas_div.style.backgroundColor = '#A800D9'
 
 function taskSelector(event){
   event.preventDefault()
-  if(event.target.value === "null") return
+  if(event.target.value === 'null') return
 
   switch(event.target.value){
-    case "työ":
-      document.getElementById('työ').style.display = "block"; break;
-    case "nostotyö": break;
-    case "nostotyö_ja_teho": break;
+    case 'työ':
+      document.getElementById('työ').style.display = 'block'
+      document.getElementById('nostotyö').style.display = 'none'
+      document.getElementById('nostotyö_ja_teho').style.display = 'none'
+      break
+    case 'nostotyö':
+      document.getElementById('työ').style.display = 'none'
+      document.getElementById('nostotyö').style.display = 'block'
+      document.getElementById('nostotyö_ja_teho').style.display = 'none'
+      break
+    case 'nostotyö_ja_teho':
+      document.getElementById('työ').style.display = 'none'
+      document.getElementById('nostotyö').style.display = 'none';
+      document.getElementById('nostotyö_ja_teho').style.display = 'block'
+      break
   }
 }
